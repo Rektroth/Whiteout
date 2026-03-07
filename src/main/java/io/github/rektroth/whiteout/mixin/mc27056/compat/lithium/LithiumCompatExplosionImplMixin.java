bf85d57b19aa6bc2eddf02d81c19b831fa1cb2a2
Rektroth <brian.rexroth.jr@gmail.com>
2026-03-07 00:43:50 -0500

    port MC-27056 patch

--- src/main/java/io/github/rektroth/whiteout/mixin/mc27056/compat/lithium/LithiumCompatExplosionImplMixin.java
+++ src/main/java/io/github/rektroth/whiteout/mixin/mc27056/compat/lithium/LithiumCompatExplosionImplMixin.java
@@ -1,21 +1,14 @@
-/*
- * Patch for MC-27056
- *
- * Authored for CraftBukkit/Spigot by commandblockguy <commandblockguy1@gmail.com> on August 14, 2020.
- * Ported to Fabric by Rektroth <brian.rexroth.jr@gmail.com> on April 24, 2024.
- */
-
 package io.github.rektroth.whiteout.mixin.mc27056.compat.lithium;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.PistonHeadBlock;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.PistonBlockEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.explosion.ExplosionImpl;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ServerExplosion;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.piston.PistonHeadBlock;
+import net.minecraft.world.level.block.piston.PistonMovingBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,14 +17,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 
-@Mixin(ExplosionImpl.class)
+@Mixin(ServerExplosion.class)
 public abstract class LithiumCompatExplosionImplMixin {
     @Final
     @Shadow
-    private ServerWorld world;
+    private ServerLevel level;
 
     @Shadow
-    protected abstract List<BlockPos> getBlocksToDestroy();
+    protected abstract List<BlockPos> calculateExplodedPositions();
 
     /**
      * I imagine this causes a massive slow-down, but the only way I can think to get around the fact that Lithium
@@ -42,19 +35,19 @@ public abstract class LithiumCompatExplosionImplMixin {
      * get around this problem.
      * @return List of blocks to destroy along with any headless pistons found.
      */
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/explosion/ExplosionImpl;getBlocksToDestroy()Ljava/util/List;"), method = "explode")
-    private List<BlockPos> getBlocksAndHeadlessPistonsToDestroy(ExplosionImpl instance) {
-        List<BlockPos> list = this.getBlocksToDestroy();
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ServerExplosion;calculateExplodedPositions()Ljava/util/List;"), method = "explode")
+    private List<BlockPos> getBlocksAndHeadlessPistonsToDestroy(ServerExplosion instance) {
+        List<BlockPos> list = this.calculateExplodedPositions();
 
         for (BlockPos pos : list) {
-            BlockState state = this.world.getBlockState(pos);
+            BlockState state = this.level.getBlockState(pos);
 
             if (state.getBlock() == Blocks.MOVING_PISTON) {
-                BlockEntity extension = this.world.getBlockEntity(pos);
+                BlockEntity extension = this.level.getBlockEntity(pos);
 
-                if (extension instanceof PistonBlockEntity blockEntity && blockEntity.isSource()) {
-                    Direction direction = state.get(PistonHeadBlock.FACING);
-                    list.add(pos.offset(direction.getOpposite()));
+                if (extension instanceof PistonMovingBlockEntity blockEntity && blockEntity.isSourcePiston()) {
+                    Direction direction = state.getValue(PistonHeadBlock.FACING);
+                    list.add(pos.relative(direction.getOpposite()));
                 }
             }
         }
