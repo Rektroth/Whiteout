d9c1a5e0dcecbf01749dd83b339ad007e0f53221
Rektroth <brian.rexroth.jr@gmail.com>
2026-03-07 10:54:50 -0500

    port MC-44654 patch

--- src/main/java/io/github/rektroth/whiteout/mixin/mc44654/EntityMixin.java
+++ src/main/java/io/github/rektroth/whiteout/mixin/mc44654/EntityMixin.java
@@ -1,7 +1,7 @@
 package io.github.rektroth.whiteout.mixin.mc44654;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,13 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Entity.class)
 public abstract class EntityMixin {
     @Shadow
-    public boolean velocityDirty;
+    public boolean needsSync;
 
     @Shadow
     public abstract EntityType<?> getType();
 
     /**
-     * Tells the game that the entity has a non-zero velocity if the tick interval is equal to the 32-bit integer limit.
+     * Tells the game that the entity has a non-zero velocity if the update interval is equal to the 32-bit integer limit.
      * (I absolutely hate the way this injection is done, but I'm not sure there's a more elegant way to do it
      * without copying code from the setPos() method.)
      * @param x  boilerplate
@@ -29,19 +29,19 @@ public abstract class EntityMixin {
     @Inject(
         at = @At(
             ordinal = 0,
-            target = "Lnet/minecraft/entity/Entity;isRemoved()Z",
+            target = "Lnet/minecraft/world/entity/Entity;isRemoved()Z",
             shift = At.Shift.AFTER,
             value = "JUMP"),
-        method = "setPos",
+        method = "setPosRaw",
         slice = @Slice(
             from = @At(
-                target = "Lnet/minecraft/world/entity/EntityChangeListener;updateEntityPosition()V",
+                target = "Lnet/minecraft/world/level/entity/EntityInLevelCallback;onMove()V",
                 value = "INVOKE"),
             to = @At(value = "RETURN")
     ))
-    private void hasVelocityIfUpdateIntervalIsMax(double x, double y, double z, CallbackInfo ci) {
-        if (this.getType().getTrackTickInterval() == Integer.MAX_VALUE) {
-            this.velocityDirty = true;
+    private void hasVelocityIfUpdateIntervalIsMax(final double x, final double y, final double z, CallbackInfo ci) {
+        if (this.getType().updateInterval() == Integer.MAX_VALUE) {
+            this.needsSync = true;
         }
     }
 }
