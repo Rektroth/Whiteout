a080fb9be93160a770f08b75f824a59a98d6aa86
Rektroth <brian.rexroth.jr@gmail.com>
2026-03-07 13:28:40 -0500

    port MC-123848 patch

--- src/main/java/io/github/rektroth/whiteout/mixin/mc123848/ItemFrameEntityMixin.java
+++ src/main/java/io/github/rektroth/whiteout/mixin/mc123848/ItemFrameEntityMixin.java
@@ -1,44 +1,38 @@
-/*
- * Patch for MC-123848
- *
- * Authored for CraftBukkit/Spigot by Jake Potrebic <jake.m.potrebic@gmail.com> on July 11, 2022.
- * Ported to Fabric by Rektroth <brian.rexroth.jr@gmail.com> on April 25, 2024.
- */
-
 package io.github.rektroth.whiteout.mixin.mc123848;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.decoration.AbstractDecorationEntity;
-import net.minecraft.entity.decoration.ItemFrameEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.decoration.HangingEntity;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(ItemFrameEntity.class)
-public abstract class ItemFrameEntityMixin extends AbstractDecorationEntity {
+/**
+ * Item frame modifications for MC-123848 patch.
+ */
+@Mixin(ItemFrame.class)
+public abstract class ItemFrameEntityMixin extends HangingEntity {
     /**
      * boilerplate
      * @param entityType boilerplate
-     * @param world      boilerplate
+     * @param level      boilerplate
      */
-    protected ItemFrameEntityMixin(EntityType<? extends AbstractDecorationEntity> entityType, World world) {
-        super(entityType, world);
+    protected ItemFrameEntityMixin(EntityType<? extends HangingEntity> entityType, Level level) {
+        super(entityType, level);
     }
 
     /**
      * Overrides the parent method to drop the item stack as an entity *below* the item frame when facing down.
-     * @param world   The world the item frame is in.
-     * @param stack   The item stack.
-     * @param yOffset The y-level offset from the item frame where the item should drop.
+     * @param level The world the item frame is in.
+     * @param stack The item stack.
      * @return The item entity to drop.
      */
-    @Nullable
     @Override
-    public ItemEntity dropStack(ServerWorld world, ItemStack stack, float yOffset) {
-        return super.dropStack(world, stack, getMovementDirection().equals(Direction.DOWN) ? yOffset - 0.6F : yOffset);
+    public ItemEntity spawnAtLocation(final @NonNull ServerLevel level, final @NonNull ItemStack stack) {
+        return super.spawnAtLocation(level, stack, getDirection().equals(Direction.DOWN) ? 0.6F : 0.0F);
     }
 }
