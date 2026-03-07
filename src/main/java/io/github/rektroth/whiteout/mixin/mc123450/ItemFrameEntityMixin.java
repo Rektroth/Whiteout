e1839560223afce45dfe408bea0b621ff71ca2dd
Rektroth <brian.rexroth.jr@gmail.com>
2026-03-07 13:13:43 -0500

    port MC-123450 patch

--- src/main/java/io/github/rektroth/whiteout/mixin/mc123450/ItemFrameEntityMixin.java
+++ src/main/java/io/github/rektroth/whiteout/mixin/mc123450/ItemFrameEntityMixin.java
@@ -1,20 +1,16 @@
-/*
- * Patch for MC-123450
- *
- * Authored for CraftBukkit/Spigot by Phoenix616 <mail@moep.tv> on November 5, 2022.
- * Ported to Fabric by Rektroth <brian.rexroth.jr@gmail.com> on April 27, 2024.
- */
-
 package io.github.rektroth.whiteout.mixin.mc123450;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.entity.decoration.ItemFrameEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ItemFrameEntity.class)
+/**
+ * Item frame modifications for MC-123450 patch.
+ */
+@Mixin(ItemFrame.class)
 public abstract class ItemFrameEntityMixin {
     /**
      * Redirects checking if the item frame is empty to check both that and if the item frame was updated,
@@ -24,8 +20,8 @@ public abstract class ItemFrameEntityMixin {
      * @return True if the item frame should play its place sound, false otherwise.
      */
     @Redirect(
-        at = @At(target = "Lnet/minecraft/item/ItemStack;isEmpty()Z", value = "INVOKE", ordinal = 1),
-        method = "setHeldItemStack(Lnet/minecraft/item/ItemStack;Z)V"
+        at = @At(target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z", value = "INVOKE", ordinal = 1),
+        method = "setItem(Lnet/minecraft/world/item/ItemStack;Z)V"
     )
     private boolean shouldPlaySound(ItemStack value, @Local(argsOnly = true) boolean update) {
         return !(!value.isEmpty() && update);
