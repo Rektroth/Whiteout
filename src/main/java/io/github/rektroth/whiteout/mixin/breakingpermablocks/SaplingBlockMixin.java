d6a3851dcfe0ab59c2fe998e2a71009bd7ae34f8
Rektroth <brian.rexroth.jr@gmail.com>
2026-03-07 00:14:51 -0500

    undo some changes to the sapling fix

--- src/main/java/io/github/rektroth/whiteout/mixin/breakingpermablocks/SaplingBlockMixin.java
+++ src/main/java/io/github/rektroth/whiteout/mixin/breakingpermablocks/SaplingBlockMixin.java
@@ -5,53 +5,40 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SaplingBlock.class)
 public abstract class SaplingBlockMixin {
 	/**
-	 * Captures tree generation before the sapling grows.
-	 * @param level  The world the sapling is in.
-	 * @param pos    The position of the sapling.
-	 * @param state  The state of the sapling.
-	 * @param random Random tick source.
-	 * @param ci     boilerplate
+	 * Captures tree generation when the sapling grows.
+	 * @param instance  The tree grower.
+	 * @param level     The world the sapling is in.
+	 * @param generator boilerplate
+	 * @param pos       The position of the sapling.
+	 * @param state     The state of the sapling.
+	 * @param random    Random tick source.
+	 * @return True if a tree generates, false otherwise.
 	 */
-	@Inject(
+	@Redirect(
 		at = @At(
 			target = "Lnet/minecraft/world/level/block/grower/TreeGrower;growTree(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/util/RandomSource;)Z",
 			value = "INVOKE"
 		),
 		method = "advanceTree"
 	)
-	private void setCaptureTreeGenerationTrue(ServerLevel level, BlockPos pos, BlockState state, RandomSource random, CallbackInfo ci) {
+	private boolean doNotDestroyPermanentBlocksWhenGrowing(TreeGrower instance, ServerLevel level, ChunkGenerator generator, BlockPos pos, BlockState state, RandomSource random) {
 		if (((CaptureTreeGenerationAccessor)level).whiteout$getCaptureTreeGeneration()) {
+			return instance.growTree(level, level.getChunkSource().getGenerator(), pos, state, random);
+		} else {
 			((CaptureTreeGenerationAccessor)level).whiteout$setCaptureTreeGeneration(true);
-		}
-	}
-
-	/**
-	 * Captures tree generation after the sapling grows.
-	 * @param level  The world the sapling is in.
-	 * @param pos    The position of the sapling.
-	 * @param state  The state of the sapling.
-	 * @param random Random tick source.
-	 * @param ci     boilerplate
-	 */
-	@Inject(
-		at = @At(
-			target = "Lnet/minecraft/world/level/block/grower/TreeGrower;growTree(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/util/RandomSource;)Z",
-			value = "INVOKE_ASSIGN"
-		),
-		method = "advanceTree"
-	)
-	private void setCaptureTreeGenerationFalse(ServerLevel level, BlockPos pos, BlockState state, RandomSource random, CallbackInfo ci) {
-		if (((CaptureTreeGenerationAccessor)level).whiteout$getCaptureTreeGeneration()) {
+			boolean b = instance.growTree(level, level.getChunkSource().getGenerator(), pos, state, random);
 			((CaptureTreeGenerationAccessor)level).whiteout$setCaptureTreeGeneration(false);
+			return b;
 		}
 	}
 }
