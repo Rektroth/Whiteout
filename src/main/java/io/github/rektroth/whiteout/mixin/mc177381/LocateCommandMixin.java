01f1fe60e10c184b539e6d25ca60cdcdeecd39a1
Rektroth <brian.rexroth.jr@gmail.com>
2026-03-08 20:56:49 -0400

    port MC-177381 patch

--- src/main/java/io/github/rektroth/whiteout/mixin/mc177381/LocateCommandMixin.java
+++ src/main/java/io/github/rektroth/whiteout/mixin/mc177381/LocateCommandMixin.java
@@ -1,11 +1,14 @@
 package io.github.rektroth.whiteout.mixin.mc177381;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.server.command.LocateCommand;
+import net.minecraft.server.commands.LocateCommand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+/**
+ * Locate command modifications for MC-177381 patch.
+ */
 @Mixin(LocateCommand.class)
 public class LocateCommandMixin {
 	/**
@@ -16,8 +19,8 @@ public class LocateCommandMixin {
 	 * @return      The hypotenuse of i and j.
 	 */
 	@Redirect(
-		at = @At(target = "Lnet/minecraft/util/math/MathHelper;sqrt(F)F", value = "INVOKE"),
-		method = "getDistance"
+		at = @At(target = "Lnet/minecraft/util/Mth;sqrt(F)F", value = "INVOKE"),
+		method = "dist"
 	)
 	private static float hypot(float value, @Local(ordinal = 4) int i, @Local(ordinal = 5) int j) {
 		return (float)Math.hypot(i, j);
