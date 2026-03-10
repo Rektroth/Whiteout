2e575943109ffb25431a1a379d35c0bd581c26ca
Rektroth <brian.rexroth.jr@gmail.com>
2026-03-10 17:11:31 -0400

    port MC-264979 patch

--- src/main/java/io/github/rektroth/whiteout/mixin/mc264979/SettingsMixin.java
+++ src/main/java/io/github/rektroth/whiteout/mixin/mc264979/SettingsMixin.java
@@ -1,6 +1,6 @@
 package io.github.rektroth.whiteout.mixin.mc264979;
 
-import net.minecraft.server.dedicated.AbstractPropertiesHandler;
+import net.minecraft.server.dedicated.Settings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,15 +10,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
 
-@Mixin(AbstractPropertiesHandler.class)
-public class AbstractPropertiesHandlerMixin {
+/**
+ * Settings modifications for MC-264979 patch.
+ */
+@Mixin(Settings.class)
+public class SettingsMixin {
 	/**
 	 * Skips loading the server.properties file if it doesn't exist in the first place.
 	 * @param path The path of the server.properties file.
 	 * @param cir  The returnable callback information.
 	 */
-	@Inject(at = @At(value = "HEAD"), method = "loadProperties", cancellable = true)
-	private static void dontLoadPropertiesIfFileDoesntExist(Path path, CallbackInfoReturnable<Properties> cir) {
+	@Inject(at = @At(value = "HEAD"), method = "loadFromFile", cancellable = true)
+	private static void doNotLoadPropertiesIfFileDoesNotExist(Path path, CallbackInfoReturnable<Properties> cir) {
 		if (!Files.exists(path)) {
 			cir.setReturnValue(new Properties());
 		}
