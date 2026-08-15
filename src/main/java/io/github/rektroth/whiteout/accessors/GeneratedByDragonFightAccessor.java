49f50407751f88385d494c3e0439b11544833ac1
Rektroth <brian.rexroth.jr@gmail.com>
2026-08-15 14:09:20 -0400

    Add additional documenting comments

--- src/main/java/io/github/rektroth/whiteout/accessors/GeneratedByDragonFightAccessor.java
+++ src/main/java/io/github/rektroth/whiteout/accessors/GeneratedByDragonFightAccessor.java
@@ -1,5 +1,8 @@
 package io.github.rektroth.whiteout.accessors;
 
+/**
+ * Accessor for the custom `setGeneratedByDragonFight` method.
+ */
 public interface GeneratedByDragonFightAccessor {
     default void whiteout$setGeneratedByDragonFight(boolean generatedByDragonFight) {
         // do nothing
